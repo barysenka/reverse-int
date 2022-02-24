b32f3fc0cff435e51d13e82f6cda47c67a22a1df
Barysenka <iborisenkoip@gmail.com>
2022-02-24 18:00:49 +0100

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let str;
+    if (n < 0){
+ str  = String(n).slice(1)
+    }else{
+    str = String(n)
+    }
+    let result =  Number(str.split('').reverse().join(''));
+    
+    
+    return result
 }
